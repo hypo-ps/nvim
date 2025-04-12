9323a549d94f96516b469c7ef3964c39bc7d2a8d
Shubham Pradhan <spradhan@clari.com>
2025-04-12 10:54:02 +0000

    added inline error diagnostics

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -4,7 +4,7 @@ return {
         config = function()
             -- setup mason with default properties
             require("mason").setup()
-        end
+        end,
     },
     -- mason lsp config utilizes mason to automatically ensure lsp servers you want installed are installed
     {
@@ -12,9 +12,9 @@ return {
         config = function()
             -- ensure that we have lua language server, typescript launguage server, java language server, and java test language server are installed
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "jdtls" },
+                ensure_installed = { "lua_ls", "jdtls", "gopls", "clangd" },
             })
-        end
+        end,
     },
     -- mason nvim dap utilizes mason to automatically ensure debug adapters you want installed are installed, mason-lspconfig will not automatically install debug adapters for us
     {
@@ -22,16 +22,16 @@ return {
         config = function()
             -- ensure the java debug adapter is installed
             require("mason-nvim-dap").setup({
-                ensure_installed = { "java-debug-adapter", "java-test" }
+                ensure_installed = { "java-debug-adapter", "java-test" },
             })
-        end
+        end,
     },
     -- utility plugin for configuring the java language server for us
     {
         "mfussenegger/nvim-jdtls",
         dependencies = {
             "mfussenegger/nvim-dap",
-        }
+        },
     },
     {
         "neovim/nvim-lspconfig",
@@ -40,7 +40,13 @@ return {
             local lspconfig = require("lspconfig")
 
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
+            vim.diagnostic.config({
+                virtual_text = true,
+                signs = true,
+                underline = true,
+                update_in_insert = false,
+                severity_sort = true,
+            })
             -- setup the lua language server
             lspconfig.lua_ls.setup({
                 capabilities = capabilities,
@@ -50,7 +56,7 @@ return {
                     filetypes = { "lua" },
                     settings = {
                         Lua = {
-                            diagnostics = { globals = { 'vim' } },
+                            diagnostics = { globals = { "vim" } },
                             workspace = {
                                 library = vim.api.nvim_get_runtime_file("", true),
                                 checkThirdParty = false,
@@ -73,11 +79,9 @@ return {
                 },
                 filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
                 root_dir = function(fname)
-                    return require("lspconfig").util.root_pattern(
-                        "compile_commands.json",
-                        "compile_flags.txt",
-                        ".git"
-                    )(fname) or vim.fn.getcwd()
+                    return require("lspconfig").util.root_pattern("compile_commands.json", "compile_flags.txt", ".git")(
+                        fname
+                    ) or vim.fn.getcwd()
                 end,
             })
             -- added for go lsp
@@ -96,21 +100,19 @@ return {
                 capabilities = require("cmp_nvim_lsp").default_capabilities(),
             })
             for server_name, server_settings in pairs(servers) do
-                local config = vim.tbl_deep_extend("force",
-                    { capabilities = capabilities },
-                    server_settings or {}
-                )
+                local config = vim.tbl_deep_extend("force", { capabilities = capabilities }, server_settings or {})
                 lspconfig[server_name].setup(config)
             end
 
-
-            vim.api.nvim_create_autocmd('LspAttach', {
+            vim.api.nvim_create_autocmd("LspAttach", {
                 callback = function(args)
                     local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-                    if not client then return end
+                    if not client then
+                        return
+                    end
                     -- Auto-format ("lint") on save.
-                    if client:supports_method('textDocument/formatting') then
-                        vim.api.nvim_create_autocmd('BufWritePre', {
+                    if client:supports_method("textDocument/formatting") then
+                        vim.api.nvim_create_autocmd("BufWritePre", {
                             buffer = args.buf,
                             callback = function()
                                 vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
@@ -126,15 +128,23 @@ return {
             -- Set vim motion for <Space> + c + a for display code action suggestions for code diagnostics in both normal and visual mode
             vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "[C]ode [A]ctions" })
             -- Set vim motion for <Space> + c + r to display references to the code under the cursor
-            vim.keymap.set("n", "<leader>gr", require("telescope.builtin").lsp_references,
-                { desc = "[C]ode Goto [R]eferences" })
+            vim.keymap.set(
+                "n",
+                "<leader>gr",
+                require("telescope.builtin").lsp_references,
+                { desc = "[C]ode Goto [R]eferences" }
+            )
             -- Set vim motion for <Space> + c + i to display implementations to the code under the cursor
-            vim.keymap.set("n", "<leader>gi", require("telescope.builtin").lsp_implementations,
-                { desc = "[C]ode Goto [I]mplementations" })
+            vim.keymap.set(
+                "n",
+                "<leader>gi",
+                require("telescope.builtin").lsp_implementations,
+                { desc = "[C]ode Goto [I]mplementations" }
+            )
             -- Set a vim motion for <Space> + c + <Shift>R to smartly rename the code under the cursor
             vim.keymap.set("n", "<leader>cR", vim.lsp.buf.rename, { desc = "[C]ode [R]ename" })
             -- Set a vim motion for <Space> + c + <Shift>D to go to where the code/object was declared in the project (class file)
             vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, { desc = "[C]ode Goto [D]eclaration" })
-        end
-    }
+        end,
+    },
 }
